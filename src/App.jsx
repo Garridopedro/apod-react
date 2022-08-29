dc255c1e6768eb308be19ada05d9374c089e607d
Garridopedro <pe.santos.garrido@gmail.com>
2022-08-29 19:52:41 -0300

    Implementando reset css e criando page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from '../../Components/Title/Title'
+
+const Home = () => {
+  return (
+    <Title texto='O CÉU NÃO É O LIMITE'/>
+  )
+}
+
+export default Home
